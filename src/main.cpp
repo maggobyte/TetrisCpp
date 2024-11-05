abf9a643940391e9396f5bff4cf072b1dac162ac
maggobyte <magnust97@gmail.com>
2024-11-05 21:57:28 +0100

    add Block class and various block implementations for grid rendering

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <raylib.h>
 #include "grid.h"
+#include "blocks.cpp"
 
 int main() 
 {
@@ -10,12 +11,14 @@ int main()
     Grid grid = Grid();
     grid.Print();
 
+    LBlock block = LBlock();
+
     while (WindowShouldClose() == false)
     {
         BeginDrawing();
         ClearBackground(darkBlue);
         grid.Draw();
-
+        block.Draw();
         EndDrawing();
     }
     
