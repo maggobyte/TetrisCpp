a77c1db0903c60b610360a5f70733be1d8df6f59
maggobyte <magnust97@gmail.com>
2024-11-05 19:15:11 +0100

    add Grid class and implement basic window setup for Tetris game

---
+++ src/grid.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class Grid
+{
+public:
+    Grid();
+private:
+    int numRows;
+    int numCols;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,18 @@
 
 int main() 
 {
+    Color darkBlue = {44, 44, 127, 255};
+    InitWindow(300, 600, "Tetris cpp");
+    SetTargetFPS(60);
 
+    while (WindowShouldClose() == false)
+    {
+        BeginDrawing();
+        ClearBackground(darkBlue);
+        
+        EndDrawing();
+    }
+    
+
+    CloseWindow();
 }
