3416fbe57fdddc7573f755cc971d3d500a7bfcbd
maggobyte <magnust97@gmail.com>
2024-11-05 21:58:05 +0100

    refactor: remove redundant cell definitions in OBlock class

--- src/blocks.cpp
+++ src/blocks.cpp
@@ -43,9 +43,6 @@ class OBlock :public Block
     {
         id = 4;
         cells[0] = {Position(0, 0), Position(0,1), Position(1,1), Position(1,1)};
-        cells[1] = {Position(0, 0), Position(0,1), Position(1,1), Position(1,1)};
-        cells[2] = {Position(0, 0), Position(0,1), Position(1,1), Position(1,1)};
-        cells[3] = {Position(0, 0), Position(0,1), Position(1,1), Position(1,1)};
     }
 };
 
