3ec570b6c9bf966fcbd4641c9182579425ff6f63
maggobyte <magnust97@gmail.com>
2024-11-06 22:05:17 +0100

    add: implement timed block movement functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include <raylib.h>
 #include "game.h"
 
+double lastUpdateTime = 0;
+
+bool EventTriggered(double interval)
+{
+    double currentTime = GetTime();
+    if(currentTime - lastUpdateTime >= interval)
+    {
+        lastUpdateTime = currentTime;
+        return true;
+    }
+    return false;
+}
+
 int main() 
 {
     Color darkBlue = {44, 44, 127, 255};
@@ -12,6 +25,10 @@ int main()
     while (WindowShouldClose() == false)
     {
         game.HandleInput();
+        if(EventTriggered(0.2))
+        {
+            game.MoveBlockDown();
+        }
         BeginDrawing();
         ClearBackground(darkBlue);
         game.Draw();
